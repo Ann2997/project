cdad580d1913dac47e3fb12636f552aa05b68769
AnnaV <w214651303@student.hccs.edu>
2026-02-23 11:02:43 -0600

    display pr by id

--- read-update-products.py
+++ read-update-products.py
@@ -27,5 +27,23 @@ def read_products():
         for r in rows
     ]
 
+@router.get("/products/{product_id}")
+def read_product(product_id: int):
+    with connect_db() as conn:
+        row = conn.execute("""
+            SELECT product_id, sku, name, description, active, created_at, updated_at
+            FROM inventory.products
+            WHERE product_id = %s
+        """, (product_id,)).fetchone()
+
+    return {
+        "product_id": row[0],
+        "sku": row[1],
+        "name": row[2],
+        "description": row[3],
+        "active": row[4],
+        "created_at": row[5],
+        "updated_at": row[6],
+    }
 
         
